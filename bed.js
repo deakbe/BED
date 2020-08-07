30eddeeebc8f1a275c40528aded5b4bd6dbacd87
deakbe <deakbe@gmail.com>
2020-08-07 15:41:24 +0200

    calculation

--- bed.js
+++ bed.js
@@ -1,4 +1,8 @@
 function bedCalc(){
-    let value =document.querySelector("input[name='dose']");
-    console.log(value);
+    let frac = document.querySelector(".frac").value;
+    let dose = document.querySelector(".dose").value;
+    let bed =document.querySelector("span.bed");
+    console.log(parseInt(frac)+parseInt(dose));
+
+    bed.innerHTML =parseInt(frac)*parseFloat(dose)*(1+parseFloat(dose)/10);
 }
