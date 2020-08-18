e1073b844fde6f7f73106100e7cd82147b9103b5
deakbe <deakbe@gmail.com>
2020-08-18 18:33:35 +0200

    DOM has been added!

--- bed.js
+++ bed.js
@@ -1,62 +1,27 @@
-let iD = ["aB1", "aB2", "aB3", "aB4", "aB5", "aB6", "aB7", "aB8", "aB9", "aB10", "aB11", "aB12"];
-let alphabeta = ["10", "3.3", "2.1", "3", "3", "4", "3", "2", "4.3", "12.3", "1.7", "3"];
-
-let object = [{
-    "id" : "aB1",
-    "value" : "10"
+let alphaData = [{
+    "id" : "aB1","value" : "10"
 },{
-    "id" : "aB2",
-    "value" : "3.3"
+    "id" : "aB2","value" : "3.3"
 },{
-    "id" : "aB2",
-    "value" : "2.1"
+    "id" : "aB3","value" : "2.1"
 },{
-    "id" : "aB3",
-    "value" : "3"
+    "id" : "aB4","value" : "3"
 },{
-    "id" : "aB4",
-    "value" : "3"
+    "id" : "aB5","value" : "3"
 },{
-    "id" : "aB5",
-    "value" : "4"
+    "id" : "aB6","value" : "4"
 },{
-    "id" : "aB6",
-    "value" : "3"
+    "id" : "aB7","value" : "3"
 },{
-    "id" : "aB7",
-    "value" : "2"
+    "id" : "aB8","value" : "2"
 },{
-    "id" : "aB8",
-    "value" : "4.3"
+    "id" : "aB9","value" : "4.3"
 },{
-    "id" : "aB9",
-    "value" : "12.3"
+    "id" : "aB10","value" : "12.3"
 },{
-    "id" : "aB10",
-    "value" : "1.7"
+    "id" : "aB11", "value" : "1.7"
 },{
-    "id" : "aB11",
-    "value" : "3"
-},{
-    "id" : "aB12",
-    "value" : "10"
-}];
-
-
-/*function alphaBeta(string) {
-    let value = 0;
-    
-    for(let i = 0; i <= iD.length; i++){
-        if(string == iD[i]){
-            value = document.querySelector("#"+iD[i]).value;
-            break;
-        }
-    }
-
-    document.querySelector(".storage").value = parseFloat(value);
-
-    return value;
-}*/
+    "id" : "aB12", "value" : "3"}];
 
 function bedCalc(){
     let alpha = document.querySelector(".storage").value;
@@ -93,77 +58,38 @@ function doseCalc() {
     return (Math.sqrt(parseFloat(frac)*parseFloat(frac)+4*parseFloat(frac)/alpha*bedCalc())-parseFloat(frac))/(2*parseFloat(frac)/alpha);
 }
 
+let createAlphaInput = (parent, i) => {
+    let div = document.createElement("div");
+    let input = document.createElement("input");
+    let td = document.createElement("td");
 
+    input.setAttribute("type", "radio");
+    input.setAttribute("id", alphaData[i].id);
+    input.setAttribute("value", alphaData[i].value);
+    input.setAttribute("name", "DOM");
 
-let table = document.querySelector(".dom1");
-for(let i = 0; i < object.lenght;i++) {
+    input.addEventListener("click", function() {
+
+        let value = document.querySelector("#"+alphaData[i].id).value;
+        document.querySelector(".storage").value = parseFloat(value);
+        return value;
+    });
+
+    div.appendChild(input);
+    td.appendChild(div);
+    parent.appendChild(td);
+}
+
+let table = document.querySelector(".dom");
+for(let i = 0; i < alphaData.length; i++) {
     let tr = document.createElement("tr");
 
-    for(let data of Object.values(object[i])) {
+    for(let data of Object.values(alphaData[i])) {
         let td = document.createElement("td");
         td.innerHTML = data;
         tr.appendChild(td);
     }
-       
-table.appendChild(tr);
+
+    createAlphaInput(tr,i);    
+    table.appendChild(tr);
 }
-
-/*let table = document.querySelector(".dom1");
-for(let i = 0; i < iD.length;i++) {
-    let tr = document.createElement("tr");
-
-    for(let j = 0; j < 2;j++) {
-        
-        let td = document.createElement("td");
-        let input = document.createElement("input");
-        let form = document.createElement("form");
-        
-        td.innerHTML = iD[i];
-        form.innerHTML = input;
-        td.innerHTML = form;
-            
-        input.setAttribute("type", "radio");
-        input.setAttribute("id", iD[i]);
-        input.setAttribute("value", alphabeta[i]);
-        input.setAttribute("name", "DOM")
-            
-        input.addEventListener("click", function() {
-            let value = 0;
-            let string = this.id;
-            value = document.querySelector("#"+this.id).value;
-            document.querySelector(".storage").value = parseFloat(value);
-        
-            return value;
-        });
-        tr.appendChild(td);
-
-    }
-
-       
-table.appendChild(tr);
-}*/
-
-/*let x = document.querySelector(".dom");
-for(let i = 0; i < iD.length; i++) {
-    let label = document.createElement("label");
-    let input = document.createElement("input");
-    let br =document.createElement("br")
-    label.innerHTML = iD[i];
-    input.setAttribute("type", "radio");
-    input.setAttribute("id", iD[i]);
-    input.setAttribute("value", alphabeta[i]);
-    input.setAttribute("name", "DOM")
-    let string = this.id;
-    input.addEventListener("click", function() {
-        let value = 0;
-        value = document.querySelector("#"+this.id).value;
-        document.querySelector(".storage").value = parseFloat(value);
-    
-        return value;
-    });
-    x.appendChild(label);
-    x.appendChild(input);
-    x.appendChild(br);
-}*/
-
- 
